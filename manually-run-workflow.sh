31e8e9912aa0981ff43813373b9626607c87781f
NikitaGlukhi <nikita.glukhi@valor-software.com>
2024-05-13 13:27:37 +0300

    test update

--- manually-run-workflow.sh
+++ manually-run-workflow.sh
@@ -6,4 +6,4 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
   https://api.github.com/repos/NikitaGlukhi/self-education/actions/workflows/manually-run-workflow.yml/dispatches \
-  -d '{"ref": "'$branch'", "inputs": {"name":"Deploy to staging"}}'
+  -d '{"ref": "'$branch'", "inputs": {"name":"Deploy to staging", "description": "Manual test"}}'
